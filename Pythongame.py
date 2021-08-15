9465fe8dd4f2974301b6d1286a7c80bc8e0cb9cb
Shreyansh Srivastava <77330034+Future306@users.noreply.github.com>
2021-08-15 16:16:19 +0530

    Here is my first python game

---
+++ Pythongame.py
@@ -0,0 +1,31 @@
+print('Hello welecome')
+
+ans = input('Are you ready to play (yes/no): ')
+score = 0
+total_q = 4
+
+if  ans.lower() =='yes':
+    ans = input('1. Which programming i am learing at present? ')
+    if ans.lower() == 'python':
+        score += 1
+        print('Correct')
+    else:
+        print('Incorrect')
+
+
+
+    ans = input('2. From where am I learning python at present? ')
+    if ans.lower == 'great learning':
+        score += 1
+        print('Correct')
+    else:
+        print('Incorrect')
+
+
+
+    ans = input('3.What is the time I am studing ? ')
+    if ans == '10:00 PM':
+        score += 1
+        print('Correct')
+    else:
+        print('Incorrect')
